9b4b79f203235b8acb4697639e613fb002c29b5f
LaNice Powell <lanicemp@aol.com>
2019-07-08 18:24:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,5 @@ def reverse_each_word(string)
   new_string_array = string_array.collect { |word| word.reverse }
   new_string_array.join(' ')
 end 
+
+# We used the split to seperate the words in an array.  
