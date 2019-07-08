727fae272bb6ab282b2f975817afd50f8f9f55f9
LaNice Powell <lanicemp@aol.com>
2019-07-08 18:18:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(string)
   string_array = string.split
-  new_string_array = new_string_array.collect 
+  new_string_array = new_string_array.collect { |word| {word.reverse} on #{"bread".reverse}" }
+ 
 end 
